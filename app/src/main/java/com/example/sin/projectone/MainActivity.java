30d905535766296568d048d0f1e76cb41e3f5aff
Nu2l <sinthirsty@gmail.com>
2016-11-11 14:46:56 +0700

    change -compileSdkVersion to 24 -min Ver to 21
    import class loopj to MainActivity
    
    Add RestfulHelper

--- app/src/main/java/com/example/sin/projectone/MainActivity.java
+++ app/src/main/java/com/example/sin/projectone/MainActivity.java
@@ -13,6 +13,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
