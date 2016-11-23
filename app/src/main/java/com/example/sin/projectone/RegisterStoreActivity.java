4fee515599510f74a0c5758572c15585525368fd
Nu2l <sinthirsty@gmail.com>
2016-11-23 11:54:26 +0700

    Add -SignIn -RegisterStore

---
+++ app/src/main/java/com/example/sin/projectone/RegisterStoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.sin.projectone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_store);
+    }
+}
