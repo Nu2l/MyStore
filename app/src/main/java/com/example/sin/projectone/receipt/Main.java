be4c7beeec89e7f850fcd0ea253a1c6e1fe4a500
Nu2l <sinthirsty@gmail.com>
2016-12-10 20:17:34 +0700

    Receipt Container

--- app/src/main/java/com/example/sin/projectone/receipt/Main.java
+++ app/src/main/java/com/example/sin/projectone/receipt/Main.java
@@ -14,7 +14,7 @@ import com.example.sin.projectone.R;
  * Created by naki_ on 11/25/2016.
  */
 
-public class Main extends ListFragment {
+public class Main extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view  = inflater.inflate(R.layout.fragment_receipt_main, container, false);
